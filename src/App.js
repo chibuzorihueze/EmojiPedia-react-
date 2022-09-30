bdc46c417e03363cbcbffdf4fa736684dbe17491
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-30 12:23:17 +0100

    maping components practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./index.css"
-import Emoji from "./components/emoji";
+import Entry from "./components/emojiCanva";
+import emojipedia from "./components/emojipedia"
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
-      <Emoji/>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
     </div>
   );
 }
